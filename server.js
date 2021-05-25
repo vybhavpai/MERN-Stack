6887a01d00973282e79b32935342bfc1dcc8c3c8
vybhavpai <vybhavpai1999.vp@gmail.com>
2021-05-25 12:56:22 +0530

    Environmrnt Setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('API Runnning');
+});
+
+app.listen(PORT, () => {
+    console.log(`server  started on port ${PORT}`);
+});
+
+
