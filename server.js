ff27fe80f10fcae51927c694db46cbdb88bb9347
vybhavpai <vybhavpai1999.vp@gmail.com>
2021-05-28 12:39:35 +0530

    user model update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json({extended: false}));
+
 connectDB();
 
 app.get('/', (req, res) => {
