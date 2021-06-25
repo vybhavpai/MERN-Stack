9d3cf7d560d7cd4d8285acc7594dee793ab081c9
vybhavpai <vybhavpai1999.vp@gmail.com>
2021-06-25 12:09:44 +0530

    Production build

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const connectDB = require('./config/db');
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,16 +8,20 @@ app.use(express.json({extended: false}));
 
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('API Runnning');
-});
-
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set the static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`server  started on port ${PORT}`);
